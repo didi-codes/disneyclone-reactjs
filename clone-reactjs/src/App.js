7ad4373194849b12f39377a11cd502f0551554b8
Damaris Garcia <dgarciaprogrammer@gmail.com>
2021-12-03 14:17:38 -0500

    created details page finishing up by adding description and details

--- clone-reactjs/src/App.js
+++ clone-reactjs/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import Header from './components/Header'
-import Home from './components/Home'
+import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Details from './components/Details';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/details'element={<Details />}/>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ clone-reactjs/src/components/Details.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Details() {
+    return (
+        <Container>
+            <Background>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="bao" />
+            </Background>
+            <ImageTitle>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt=""/>
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+                    <img src="/images/play-icon-black.png" alt="play"/>
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>
+                    <img src="/images/play-icon-white.png" alt="trailer"/>
+                    <span>Trailer</span>
+                </TrailerButton>
+                <AddButton>
+                    <span>+</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="/images/group-icon.png" alt="group watch" />
+                </GroupWatchButton>
+            </Controls>
+        </Container>
+    )
+}
+
+export default Details
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+`
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    opacity: 0.8;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
+const ImageTitle = styled.div`
+    height: 30vh;
+    width: 35vw;
+    min-height: 170px;
+    min-width: 200px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+`
+const Controls = styled.div`
+    display:flex;
+    align-items: center;
+
+`
+const PlayButton = styled.div`
+    border-radius: 4px;
+    font-size: 15px;
+    padding: 0 24px;
+    margin-right: 22px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb(249, 249, 249);
+    border: none;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+    color: #000;
+
+    &:hover {
+       background: rgb(198, 198, 198);
+    }
+`
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+    text-transform: uppercase;
+`
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid #fff;
+    background-color: rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+
+    span {
+        font-size: 30px;
+        color: #fff;
+    }
+`
+const GroupWatchButton = styled(AddButton)`
+    background: rgb(0, 0, 0)
+`
